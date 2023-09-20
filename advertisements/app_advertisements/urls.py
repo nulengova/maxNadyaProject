b9e8abf9d7335353e3d20bcdd83d106c819b105e
Надя <nulengova@gmail.com>
2023-09-20 12:11:00 +0700

    Перовый комит

---
+++ advertisements/app_advertisements/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import index, top_sellers
+
+urlpatterns = [
+    path('',index, name = 'main-page'),
+    path('top-sellers.html/', top_sellers, name = 'top-sellers'),
+]
