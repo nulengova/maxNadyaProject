96c56eb41c7e0025e47b7ddb50642d4c3415a5c5
Надя <nulengova@gmail.com>
2023-09-20 13:02:14 +0700

    Второй комит

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import index, top_sellers
+from .views import index, top_sellers, register,login,profile, advertisement, post
 
 urlpatterns = [
     path('',index, name = 'main-page'),
     path('top-sellers.html/', top_sellers, name = 'top-sellers'),
+    path('register.html',register, name = 'register'),
+    path('login.html',login, name = 'login'),
+    path('profile.html',profile, name = 'profile'),
+    path('advertisement-post.html',advertisement, name = 'advertisement'),
+    path('advertisement-post.html',post, name = 'post' )
 ]
