83f1ca2f4948ef969066fa9798931c39043cdbc2
Надя <nulengova@gmail.com>
2023-10-04 14:05:01 +0700

    6 коммит

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
-
+from django.contrib.auth import get_user_model
+User = get_user_model()
 class Advertisement(models.Model):
     title = models.CharField("Заголовок", max_length=128)
     description = models.TextField("Описание")
@@ -9,6 +10,9 @@ class Advertisement(models.Model):
     auction = models.BooleanField("Торг", help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name="Пользователь", on_delete= models.CASCADE, null=True)
+    image = models.ImageField('изображения', upload_to="advertisements")
+
     @admin.display(description = 'дата создания')
     def created_date(self):
         from django.utils import timezone
@@ -31,3 +35,11 @@ class Advertisement(models.Model):
             return format_html(
                 f'<span style = "color: blue; font-weight: bold;">Сегодня в {updated_time}</span>'
             )
+
+    @admin.display(description='фото')
+    def get_html_image(self):
+        if self.image:
+            return format_html(
+                '<img src="{url}" style="max-width: 80px; max-height: 80px;"', url=self.image.url
+            )
+
